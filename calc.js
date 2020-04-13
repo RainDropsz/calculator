c3fd2108275fcc9dce9de6911e7256a476a0b97e
RainDropsz <shinida@gmail.com>
2020-04-12 21:17:53 -0400

    add num object, fix backspace

--- calc.js
+++ calc.js
@@ -2,55 +2,49 @@
 var arrstr = [];
 var numstr = "";
 var shouldclear = false;
-var hasError = false;
+
 
 // Functions
-
 function clickNum(e) {
-  if (shouldclear == true) { clickClear();}
-
+  if (shouldclear == true) { clickClear(); }
   numstr += $(e.target).text();
   $(".inputline").append( $(e.target).text() );
 }
 
 function clickOp(e) {
-  $(".inputline").append( $(e.target).text() );
+  var op = $(e.target).text();
+  if (shouldclear == true) { clickClear(); }
 
-  if (isDecErr(numstr)) { hasError = true; }
+  $(".inputline").append( op  );
 
-  arrstr.push(numstr);   numstr = "";
-  arrstr.push( $(e.target).attr("id") );
-}
+  arrstr.push ( { num: numstr, isnum: !isDecErr(numstr) } );
+  arrstr.push ( { num: op,     isnum: false             } );
 
-function isDecErr(num1) {
-  var numofdecimals = num1.match(/\./g);
-
-  if (!numofdecimals)  {return false;}          // check if null
-  if (numofdecimals.length > 1) {return true;}  // more than 1 decimal
-  return false;
+  numstr = "";
 }
 
 function clickEq(e) {
   var opIndex = -1, x = 0, y = 0, ans = "";
-  const numdigits = 9;
+  if (shouldclear == true) { clickClear(); }
 
-  if (isDecErr(numstr) || hasError) {shouldclear = true; showerrarr(); return;}
+  arrstr.push ( { num: numstr, isnum: !isDecErr(numstr) } );
+  numstr = "";
 
-  arrstr.push(numstr); numstr = "";
+  if (!isarrgood()) {shouldclear = true; showerrarr(); return;}
 
   while ( arrstr.length > 2) {
     opIndex = orderOfOp();
 
-    x = parseFloat(arrstr[ opIndex - 1 ]);
-    y = parseFloat(arrstr[ opIndex + 1 ]);
-    op = arrstr[ opIndex ];
+    x = parseFloat(arrstr[ opIndex - 1 ].num);
+    y = parseFloat(arrstr[ opIndex + 1 ].num);
+    op = arrstr[ opIndex ].num;
 
     ans = operate(op, x, y);
-    arrstr.splice( opIndex - 1 , 3 , ans);
+    arrstr.splice( opIndex - 1 , 3 , {num:ans, isnum:true});
   }
 
   if (arrstr.length == 1) {
-    ans = arrstr[0];
+    ans = arrstr[0].num;
   }
 
   ans = roundNum(ans);
@@ -65,19 +59,19 @@ function orderOfOp() {
   var i = 1;
 
   for ( i = 1; i < arrstr.length; i+=2 ) {
-    if ( arrstr[i] == "times" || arrstr[i] == "divide" ) { return i; }  }
+    if ( arrstr[i].num == "×" || arrstr[i].num == "÷" ) { return i; }  }
 
   for ( i = 1; i < arrstr.length; i += 2) {
-    if ( arrstr[i] == "plus" || arrstr[i] == "minus" ) { return i;  }  }
+    if ( arrstr[i].num == "+" || arrstr[i].num == "−" ) { return i;  }  }
 
   return -1;
 }
 
 function operate( op, x, y) {
-  if      (op == "plus")   { return x + y; }
-  else if (op == "minus")  { return x - y; }
-  else if (op == "times")  { return x * y; }
-  else if (op == "divide") { return x / y; }
+  if      (op == "+")  { return x + y; }
+  else if (op == "−")  { return x - y; }
+  else if (op == "×")  { return x * y; }
+  else if (op == "÷")  { return x / y; }
 }
 
 function clickClear( ) {
@@ -86,38 +80,51 @@ function clickClear( ) {
   numstr = "";
   arrstr = [];
   shouldclear = false;
-  hasError = false;
+}
+
+function clearNumObj(x) {
+  x.num = "";
+  x.isnum = false;
+}
+
+function isarrgood() {
+  var ans = true;
+
+  for(let i = 0; i < arrstr.length; i+=2) {
+    ans = ans && arrstr[i].isnum;
+  }
+
+  return ans;
 }
 
 function backspace() {
   var txt = $(".inputline").text();
-  var arrtxt = "";
-  var ind = 0;
-  var len = arrstr.length;
+  $(".inputline").text( txt.slice(0, txt.length - 1) );
 
-  if (numstr > 0) {
-    numstr = numstr.slice(0, numstr.length - 1);
-    $(".inputline").text( txt.slice(0, txt.length - 1) ); }
-  else if (arrstr.length > 0) {
-    if(arrstr.length == 1) {
-      arrstr.pop();
-      $(".inputline").text( txt.slice(0, txt.length - 1) ); }
-    else {
-      arrtxt = arrstr[arrstr.length - 1];
-      arrtxt = arrtxt.slice(0, arrtxt.length - 1);
-      $(".inputline").text( txt.slice(0, txt.length - 1) ); }
+  if ( numstr != "") {
+    arrstr.push( { num:numstr, isnum: !isDecErr(numstr) } );
   }
 
+  var arrtxt = arrstr.pop();
+  arrtxt = arrtxt.num;
+  numstr = arrtxt.slice(0, numstr.length - 1);
+
+  if (arrtxt.length > 1) {
+    arrtxt = arrtxt.slice( 0, arrtxt.length - 1);
+    numstr = arrtxt;
+  }
 }
 
 function roundNum(ans) {
   const numdigits = 9;
 
-  ans = Math.round( ans * Math.pow(10, numdigits) ) / Math.pow(10,numdigits);
+  ans = Math.round( ans * (10 ** numdigits) ) / (10 ** numdigits);
   return ans;
 }
 
 function typeNum(e) {
+  if (shouldclear == true) { clickClear(); }
+
   if      (e.key == "1") { $("#num1").click(); }
   else if (e.key == "2") { $("#num2").click(); }
   else if (e.key == "3") { $("#num3").click(); }
@@ -145,18 +152,21 @@ function typeNum(e) {
 function showerrarr() {
   var errarr = [], ans = "";
 
-  errarr.push(    "<span style='color:black'>" + "("  + "</span>" +
-                  "<span style='color:red'  >" + "ㆆ" + "</span>" +
-                  "<span style='color:black'>" + "_"  + "</span>" +
-                  "<span style='color:red'  >" + "ㆆ" + "</span>" +
-                  "<span style='color:black'>" + ")" +  "</span>" );
-
+  errarr.push ("(ㆆ_ㆆ)");
   errarr.push ("¯" + String.fromCharCode(92) + "_(ツ)_/¯");
   errarr.push ("ಠ_ಠ");
 
   ans = errarr[ Math.floor( Math.random() * errarr.length  ) ];
   $(".ansline").html(ans);
   numstr="";
+}
+
+function isDecErr(num1) {
+  var numofdecimals = num1.match(/\./g);
+
+  if (!numofdecimals)  {return false;}          // check if null
+  if (numofdecimals.length > 1) {return true;}  // more than 1 decimal
+  return false;
 }
 
 
@@ -168,4 +178,3 @@ $(".eql")      .on("click",   (e) => clickEq   (e) );
 $("#clear")    .on("click",   ( ) => clickClear( ) );
 $("#backspace").on("click",   ( ) => backspace ( ) );
 $(document)    .on("keydown", (e) => typeNum   (e) );
-
